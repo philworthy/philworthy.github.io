b43542c6d4830338c30466f5ecbeeb6ab0ca8f26
Phil Worthington <phil@livestream.com>
2014-08-09 20:10:13 -0400

    edit

--- TrelloReport/js/controllers.js
+++ TrelloReport/js/controllers.js
@@ -13,8 +13,14 @@ appControllers.controller('BoardsController', ['$scope', 'TrelloDataService', fu
 	$scope.model.ready = false;
 }]);
 
-appControllers.controller('QACardsController', ['$scope', 'TrelloDataService', function($scope, TrelloDataService) {
+appControllers.controller('QACardsController2', ['$scope', 'TrelloDataService', function($scope, TrelloDataService) {
 	TrelloDataService.loadData($scope, 'boards/4fe9828c6b88d0c66f40ac32/lists', {  });
+	$scope.model = TrelloDataService.model();
+	$scope.model.ready = false;
+}]);
+
+appControllers.controller('QACardsController', ['$scope', 'TrelloDataService', function($scope, TrelloDataService) {
+	TrelloDataService.loadData($scope, 'cards/7Deey4QC', {  });
 	$scope.model = TrelloDataService.model();
 	$scope.model.ready = false;
 }]);
