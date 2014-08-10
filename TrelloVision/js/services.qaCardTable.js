c311abef236d723498136db1faf96e647924c8dd
Phil Worthington <phil@livestream.com>
2014-08-09 22:10:16 -0400

    LOG

--- TrelloVision/js/services.qaCardTable.js
+++ TrelloVision/js/services.qaCardTable.js
@@ -111,7 +111,7 @@ TrelloVisionApp.factory('QACardTableService', function() {
 /*----------------------------------------------------------------------------------------------------*/
 function buildQACardTable(scope) {
 
-	console.log("QA"+scope.model.data);
+	console.log("QA");
 
 	var board = scope.model.data;
 
