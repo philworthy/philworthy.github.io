2f8364177a041813dd399f06f403b76c44e6d2a5
Phil Worthington <phil@livestream.com>
2014-08-10 13:56:03 -0400

    edit

--- TrelloVision/js/services.listCardTable.js
+++ TrelloVision/js/services.listCardTable.js
@@ -38,7 +38,7 @@ TrelloVisionApp.factory('ListCardTableService', function() {
 			}
 		});*/
 
-		TrelloDataService.loadData(scope, 'cards/tWfbdlze', params, function(scope) {
+		TrelloDataService.loadData(scope, 'lists/53e37d06f2ef915cb1407bbd/cards', params, function(scope) {
 			console.log(">>>>got data back");
 			buildListCardTable(scope);
 
@@ -116,7 +116,86 @@ TrelloVisionApp.factory('ListCardTableService', function() {
 /*----------------------------------------------------------------------------------------------------*/
 function buildListCardTable(scope) {
 
-	console.log(">>>LIst:"+scope.model.data.toString());
+	console.log(">>>build list:"+scope.model.data.toString());
+
+	var board = scope.model.data;
+
+	var table = {
+		board: board,
+		org: board.organization,
+		members: board.members,
+		labelColors: ['green', 'yellow', 'orange', 'red', 'purple', 'blue'],
+		labelMap: board.labelNames,
+		listIds: [],
+		listMap: {},
+		cards: []
+	};
+
+	scope.model.table = table;
+
+	for ( var li in board.lists ) {
+		var list = board.lists[li];
+		table.listIds.push(list.id);
+		table.listMap[list.id] = list;
+	}
+
+	for ( var ci in board.cards ) {
+		var card = board.cards[ci];
+		var c = {};
+		table.cards.push(c);
+
+		c.id = card.id;
+		c.shortId = card.idShort;
+		c.listId = card.idList;
+		c.listName = table.listMap[card.idList].name;
+		c.listNameFilter = cleanFilterText(c.listName);
+		c.name = card.name;
+		c.desc = card.desc;
+		c.url = card.url;
+		c.updatedRaw = card.dateLastActivity;
+		c.updated = moment(card.dateLastActivity).format('MMM D');
+		c.dueRaw = card.due;
+		c.due = (card.due == null ? null : moment(card.due).format('MMM D'));
+		c.memberCount = card.idMembers.length;
+		c.commentCount = card.badges.comments;
+		c.voteCount = card.badges.votes;
+		c.checklists = [];
+		c.tags = '';
+		c.tagCount = 0;
+
+		for ( li in card.labels ) {
+			var lbl = card.labels[li];
+			c[lbl.color+'Label'] = lbl.name;
+		}
+
+		for ( li in card.checklists ) {
+			var list = card.checklists[li];
+			var comp = 0;
+
+			for ( i in list.checkItems ) {
+				if ( list.checkItems[i].state == 'complete' ) {
+					++comp;
+				}
+			}
+
+			c.checklists.push({
+				name: list.name,
+				progress: comp+'/'+list.checkItems.length
+			});
+		}
+
+		for ( var mi in card.idMembers ) {
+			var memId = card.idMembers[mi];
+			c['member'+memId] = true;
+		}
+
+		var match;
+
+		while ( (match = HashTagPattern.exec(c.desc)) ) {
+			c.tags += '#'+match[3]+' ';
+			c.tagCount++;
+		}
+	}
 
 }
 
