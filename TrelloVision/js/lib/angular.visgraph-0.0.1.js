821b547bac934cd77d2e68cde3100a66f4257d33
Phil Worthington <phil@livestream.com>
2014-08-12 16:09:06 -0400

    edit

--- TrelloVision/js/lib/angular.visgraph-0.0.1.js
+++ TrelloVision/js/lib/angular.visgraph-0.0.1.js
@@ -26,4 +26,28 @@ angular.module('angular.visgraph', []).directive('visGraph', [function() {
   };
 }]);
 
-
+angular.module('angular.vistimeline', []).directive('visTimeline', [function() {
+  return {
+    restrict: 'AE',
+    scope: {
+      data: '=data',
+      options: '=options',
+      event: '@event',
+      callback: '&'
+    },
+    link: function(scope, element, attrs) {        
+    var container = element[0], buildGraph = function(scope) {
+        var graph = null;
+        graph = new vis.Timeline(container, scope.data, scope.options);
+          return graph.on(scope.event, function(properties) {
+            if (properties.length !== 0) { 
+              scope.callback({params: properties});
+            } 
+        });
+      };      
+      scope.$watch('data', function(newval, oldval) {
+        buildGraph(scope);
+      }, true);        
+    }
+  };
+}]);
