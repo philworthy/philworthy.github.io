427ff912e7f0fd28816a50fdd979fc0a1c6c2011
Phil Worthington <phil@livestream.com>
2014-08-13 19:11:18 -0400

    edit

--- TrelloVision/js/services.board.js
+++ TrelloVision/js/services.board.js
@@ -96,8 +96,8 @@ TrelloVisionApp.factory('BoardService', function() {
 				var item = {
 					type: 'range', 
 					group: cardId, 
-					start: moment(previousAction.date).toDate(),
-					end: moment(endDate).toDate()
+					end: moment(previousAction.date).toDate(),
+					start: moment(endDate).toDate()
 				}
 				if(previousAction.type == "createCard") item.content = previousAction.data.list.name;
 				else if(previousAction.type == "updateCard") item.content = previousAction.data.listAfter.name;
