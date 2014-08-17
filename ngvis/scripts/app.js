b7bd62da0cf5ae34d67b38fcab980563cf762732
Phil Worthington <phil@livestream.com>
2014-08-17 09:46:19 -0400

    added ngvis

---
+++ ngvis/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular
+  .module('ngVisJsApp', [
+    'ngRoute',
+    'ngVis'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/timeline', {
+        templateUrl: 'views/timeline.html',
+        controller: 'TimelineCtrl'
+      })
+      .when('/graph', {
+        templateUrl: 'views/graph.html',
+        controller: 'GraphCtrl'
+      })
+      .otherwise({
+        redirectTo: '/timeline'
+      });
+  })
+  .run(function ($rootScope) {
+
+    $rootScope.pages = [
+      'Timeline',
+      'Graph'
+    ];
+
+  });
