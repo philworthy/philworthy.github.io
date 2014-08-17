1000dc9633bf94e2ef5b103347318bb1389d3a69
Phil Worthington <phil@livestream.com>
2014-08-16 23:53:43 -0400

    edit

--- ts/js/services.board.js
+++ ts/js/services.board.js
@@ -158,7 +158,7 @@ TrelloScheduleApp.factory('BoardService', function() {
 				timelineGroups.push({
 					id: card.id,
 					content: getStateOrder(card.state) + "-" + card.state + ": " + card.name.substring(0,30) + '..',
-					content: '<a href="{{'+card.url+'}}" target="_blank">{{'+(card.name.length>30?:card.name.substring(0,30)+"..":card.name)+'}}</a>',
+					content: '<a href="{{'+card.url+'}}" target="_blank">{{'+(card.name.length>30?:card.name.substring(0,30):card.name)+'}}</a>',
 					title: card.name,
 					order: getStateOrder(card.state) + "-" + card.due
 				});
